de667af42b0efd55084d2e3e3d2bebb30f9ed69b
Jahongir <jahongirhakimjonov@gmail.com>
2024-04-30 17:04:55 +0500

    commit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import asyncio
+import datetime
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, types
+import fitz
+from PIL import Image
+from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
-from CSVtoXLSX import process_csv
-from PDFtoJPG import process_pdf_files
-
-load_dotenv('env/.env')
+load_dotenv("env/.env")
 
 bot_token = os.getenv("BOT_TOKEN")
 if not bot_token:
@@ -23,7 +23,6 @@ dp = Dispatcher(bot)
 keyboard = InlineKeyboardMarkup(row_width=2)
 buttons = [
     InlineKeyboardButton("PDF to JPG", callback_data="pdf_to_jpg"),
-    InlineKeyboardButton("CSV to XLSX", callback_data="csv_to_xlsx"),
 ]
 keyboard.add(*buttons)
 
@@ -35,7 +34,6 @@ continue_stop_buttons = [
 continue_stop_keyboard.add(*continue_stop_buttons)
 
 pdf_queue = asyncio.Queue()
-csv_queue = asyncio.Queue()
 
 
 @dp.message_handler(commands=["start"])
@@ -47,21 +45,6 @@ async def start_command(message: types.Message):
     )
 
 
-@dp.callback_query_handler(lambda c: c.data == "csv_to_xlsx")
-async def process_csv_to_xlsx(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(
-        callback_query.from_user.id, "CSV faylni yuboring. XLSX formatiga o'tkazilmoqda."
-    )
-
-    @dp.message_handler(content_types=types.ContentType.DOCUMENT)
-    async def csv_to_xlsx(message: types.Message):
-        if message.document.mime_type == "text/csv":
-            await csv_queue.put(message)
-
-            asyncio.create_task(process_csv(bot, message))
-
-
 @dp.callback_query_handler(lambda c: c.data == "pdf_to_jpg")
 async def process_pdf_to_jpg(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
@@ -69,12 +52,54 @@ async def process_pdf_to_jpg(callback_query: types.CallbackQuery):
         callback_query.from_user.id, "JPG qilish uchun PDF faylni yuboring."
     )
 
-    @dp.message_handler(content_types=types.ContentType.DOCUMENT)
-    async def pdf_to_jpg(message: types.Message):
-        if message.document.mime_type == "application/pdf":
-            await pdf_queue.put(message)
 
-            asyncio.create_task(process_pdf_files(bot, continue_stop_keyboard))
+@dp.message_handler(content_types=types.ContentType.DOCUMENT)
+async def pdf_to_jpg(message: types.Message):
+    if message.document.mime_type == "application/pdf":
+        await pdf_queue.put(message)
+
+
+async def process_pdf_files():
+    while True:
+        message = await pdf_queue.get()
+
+        await message.answer("PDF fayl qabul qilindi. JPG formatiga o'tkazilmoqda...")
+
+        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        file_path = f"temp_{timestamp}.pdf"
+
+        file = await bot.download_file_by_id(
+            message.document.file_id, destination=file_path
+        )
+        file.close()
+
+        try:
+            doc = fitz.open(file_path)
+            for i in range(len(doc)):
+                zoom = 10.0
+                mat = fitz.Matrix(zoom, zoom)
+                pix = doc.get_page_pixmap(i, matrix=mat)
+
+                img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+                img_file_path = f"out_{timestamp}_{i}.jpg"
+                img.save(img_file_path, "JPEG", quality=100)
+
+                with open(img_file_path, "rb") as img_file:
+                    await bot.send_document(message.chat.id, img_file)
+
+                os.remove(img_file_path)
+
+            doc.close()
+
+        finally:
+            os.remove(file_path)
+
+        pdf_queue.task_done()
+        await bot.send_message(
+            message.chat.id,
+            "Jarayon yakunlandi : )",
+            reply_markup=continue_stop_keyboard,
+        )
 
 
 @dp.callback_query_handler(lambda c: c.data == "continue")
@@ -97,6 +122,7 @@ async def process_stop(callback_query: types.CallbackQuery):
 
 
 if __name__ == "__main__":
-    from aiogram import executor
-
+    Bot.set_current(bot)
+    loop = asyncio.get_event_loop()
+    loop.create_task(process_pdf_files())
     executor.start_polling(dp, skip_updates=True)
